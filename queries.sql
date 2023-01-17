d21cbd4abe052c49e0813b7cc6183fa8c695160d
Anam Khalid <anam.cheema43@gmail.com>
2023-01-17 20:08:06 +1100

    initial commit

---
+++ queries.sql
@@ -0,0 +1,32 @@
+CREATE TABLE movies (
+    movie_id integer,
+	title char(255),
+    genres char(255),
+   	PRIMARY KEY (movie_id)
+ );
+ 
+ Select * from movies;
+ 
+ CREATE TABLE ratings(
+  	user_id integer,
+	movie_id integer,
+    rating integer,
+	timestamps timestamp,
+	PRIMARY KEY (movie_id)
+);
+
+Select * from ratings
+-- Join 2 tables
+SELECT
+	movies.movie_id,
+	movies.title,
+	movies.genres,
+	ratings.user_id,
+    ratings.rating,
+	ratings.timestamps
+FROM
+	movies
+INNER JOIN
+	 ratings
+ON
+	movies.movie_id = ratings.movie_id;
